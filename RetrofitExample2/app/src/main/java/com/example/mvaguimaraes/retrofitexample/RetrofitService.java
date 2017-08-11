b5753d3d3fed08438b3f0c04546b5ae633df481d
Marcos Vinicius Almeida Guimaraes <mvaguimaraes@gmail.com>
2017-08-11 11:49:57 -0300

    Using Retrofit
    Simple conversion app using the Retrofit Library and a converter API.

---
+++ RetrofitExample2/app/src/main/java/com/example/mvaguimaraes/retrofitexample/RetrofitService.java
@@ -0,0 +1,23 @@
+package com.example.mvaguimaraes.retrofitexample;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+/**
+ * Created by Mvaguimaraes on 8/11/17.
+ */
+
+public interface RetrofitService {
+
+    @Headers("X-Mashape-Key: AuuyclCPjcmshv2iOPq190OpzLrMp1FJWwejsnJrdfwOUr4h44")
+
+    @FormUrlEncoded
+    @POST("convert")
+    Call<RespostaServidor> converterUnidade(@Field("from-type") String from_type,
+                                            @Field("from-value") String from_value,
+                                            @Field("to-type") String to_type);
+
+}
